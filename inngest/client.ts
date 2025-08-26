707eda41d4a0d53d6c27386bd73b98d4c0126054
nishasharma303 <nishaanshu2005@gmail.com>
2025-08-27 00:56:43 +0530

    added event key

--- inngest/client.ts
+++ inngest/client.ts
@@ -1,5 +1,7 @@
 import { Inngest } from "inngest";
 
-
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "career-path" });
+export const inngest = new Inngest({ 
+  id: "career-path",
+  eventKey: process.env.INNGEST_EVENT_KEY // ← ADD THIS LINE
+});
